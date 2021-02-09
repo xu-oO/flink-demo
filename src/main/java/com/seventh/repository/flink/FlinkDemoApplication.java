c953dd1284e153af38c00ce093bfe7ac3191a6c9
张旭 <yusheng_zx@163.com>
2021-02-09 16:43:25 +0800

    Initialize the project

---
+++ src/main/java/com/seventh/repository/flink/FlinkDemoApplication.java
@@ -0,0 +1,13 @@
+package com.seventh.repository.flink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FlinkDemoApplication.class, args);
+  }
+
+}
